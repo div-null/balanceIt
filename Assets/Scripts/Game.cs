c3217bb45ce2ad71ee119570f055aa04bc6af17f
Optifun <frdima19@gmail.com>
2020-07-11 11:02:17 +0700

    Some Scripts

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+	private int gameScore;
+	public int GameScore { get; set; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+	// Update is called once per frame
+	void Update()
+    {
+        
+    }
+	IEnumerable StartGame ()
+	{
+
+	yield return new WaitForSeconds(1f);
+	}
+	IEnumerable EndGame ()
+	{
+	yield return new WaitForSeconds(1f);
+	}
+
+	IEnumerable IncreaseScore ()
+	{
+	yield return new WaitForSeconds(1f);
+	}
+
+}
